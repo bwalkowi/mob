21bec85c2cbd0efded76c93791b3e317289cad61
Bartosz Walkowicz <wallko1@hotmail.com>
2019-05-22 19:08:18 +0200

    Add picker for selecting device

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Div, Text, View} from 'react-native';
+import {Platform, StyleSheet, Div, Text, View, Picker} from 'react-native';
 import { BleManager } from 'react-native-ble-plx';
 import {PermissionsAndroid} from 'react-native';
 
@@ -39,7 +39,12 @@ export default class App extends Component<Props> {
   constructor() {
     super()
     this.manager = new BleManager()
-    this.state = {info: "", values: {}}
+    this.state = {
+      info: "", 
+      foundDevices: [],
+      selectedDevice: null,
+      measurments: {}, 
+    }
 
     this.serviceUUID = "0000ABCE-1212-EFDE-1523-785FEF13D123"
     this.sensorSelectorCharacteristicUUID = "0000BEA0-1212-EFDE-1523-785FEF13D123"
@@ -67,8 +72,8 @@ export default class App extends Component<Props> {
     this.setState({info: "ERROR: " + message})
   }
 
-  updateValue(key, value) {
-    this.setState({values: {...this.state.values, [key]: value}})
+  updateMeasurments(key, value) {
+    this.setState({measurments: {...this.state.measurments, [key]: value}})
   }
 
   componentWillMount() {
@@ -84,21 +89,28 @@ export default class App extends Component<Props> {
   }
 
   startScan() {
-    this.manager.startDeviceScan([this.serviceUUID],
-                                 null, (error, device) => {
-      console.log(device)
-
-      // should be after if(error)
-      this.updateValue("SHT31", [Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1])
-
+    this.manager.startDeviceScan(null, null, (error, device) => {
       if (error) {
         this.error(error.message)
         this.startScan()
         return
       }
 
-      data = device.manufacturerData
-      this.info("Got: " + data)
+      if(device.name == "DigitalTherm"){
+        console.log(device)
+
+        if(this.state.foundDevices.indexOf(device.id) == -1){
+          this.setState({foundDevices: this.state.foundDevices.concat(device.id)})
+          return
+        }
+
+        if(device.id == this.state.selectedDevice){
+          data = device.manufacturerData
+          this.info("Got: " + data)
+
+          this.updateMeasurments("SHT31", [Math.floor(Math.random() * 100) + 1, Math.floor(Math.random() * 100) + 1])
+        }
+      }
     });
   }
 
@@ -106,12 +118,25 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <View style={styles.pickerContainer}>
+          <Text style={{fontSize: 17, margin: 10}}>Selected device: </Text>
+          <Picker
+            selectedValue={this.state.selectedDevice || "None"}
+            style={styles.picker}
+            onValueChange={(itemValue, itemIndex) =>
+              this.setState({selectedDevice: itemValue, values: {}})
+            }>
+            {this.state.foundDevices.map((mac_id) => {
+              return <Picker.Item label={mac_id} value={mac_id} />
+            })}
+          </Picker>
+        </View>
         <Text>{this.state.info}</Text>
         {Object.values(this.sensors).map((sensor) => {
             return <View key={sensor.name}>
                      <Text>{sensor.name + ":"}</Text>
                      {sensor.measurments.map((item, idx) => {
-                         return <Text>{"   " + item + ": " + (this.state.values[sensor.name] ? this.state.values[sensor.name][idx] : "-")}</Text>
+                         return <Text>{"   " + item + ": " + (this.state.measurments[sensor.name] ? this.state.measurments[sensor.name][idx] : "-")}</Text>
                      })}
                    </View>
         })}
@@ -120,6 +145,7 @@ export default class App extends Component<Props> {
   }
 
 }
+
 
 const styles = StyleSheet.create({
   container: {
@@ -132,6 +158,18 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  pickerContainer: {
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'center', 
+    backgroundColor: 'white',
+    paddingVertical: 10
+  },
+  picker: {
+    height: 50, 
+    width: 180,
+    justifyContent: 'center'
   },
   instructions: {
     textAlign: 'center',
